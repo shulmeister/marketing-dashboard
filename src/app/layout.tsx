536a548b3da3c8a473497ca450ded76abd59934c
shulmeister <jason@coloradocareassist.com>
2025-06-26 10:00:53 -0600

    Recreate layout.tsx with correct default import

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import MaterialUIControllerProvider from '../context/MaterialUIContext'
+import './globals.css'
+import type { ReactNode } from 'react'
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <MaterialUIControllerProvider>
+          {children}
+        </MaterialUIControllerProvider>
+      </body>
+    </html>
+  )
+}
